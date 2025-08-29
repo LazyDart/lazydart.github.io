8fcd589eaea6afc752efcad44919b3229fd78f57
Lazy Dart <estp.pl@gmail.com>
2025-08-29 11:31:49 +0200

    added link to blog

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
+import Link from "next/link"
+
 export default function Home() {
   return (
     <main>
-      <div>Next.js on GitHub Pages</div>
+      <div>Next.js on GitHub Pagesd</div>
+      <li key={"blog"}>
+            {/* Link navigates to /blog/[slug] */}
+            <Link href={`/blog/`}>
+              blog
+            </Link>
+        </li>
     </main>
   );
 }
