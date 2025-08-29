3868d959b1ac9f37b2672728275fe3ce343b06dc
Lazy Dart <estp.pl@gmail.com>
2025-08-29 09:57:39 +0200

    feat: perp for markdown deployment

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,22 @@
 import type { NextConfig } from 'next';
 
+const withMDX = require('@next/mdx')({
+    extension: /\.(md|mdx)$/,
+    options: {
+        remarkPlugins: [],
+        rehypePlugins: [],
+    },
+});
+
+module.exports = withMDX({
+    pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
+    images: {
+        unoptimized: true,
+    },
+    output: 'export',
+});
+
+
 const nextConfig: NextConfig = {
   output: 'export',
   basePath: process.env.PAGES_BASE_PATH,
